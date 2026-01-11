a553b98573050484f7699f60cd1bc2835bba4d7e
HeartPy <wubenxin9@gmail.com>
2026-01-12 06:34:08 +0900

    fix(business_owners): ValidationErrorのインポートを追加
    rest_framework.exceptions.ValidationErrorのインポートを追加

--- business_owners/views.py
+++ business_owners/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny, BasePermission
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AnonymousUser
